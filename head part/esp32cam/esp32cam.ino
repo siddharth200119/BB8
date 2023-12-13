fc889a3f5df7c0e1f22435dc27ea5eb3ef46d983
siddharth200119 <siddramnani@gmail.com>
2023-12-13 15:00:24 +0530

    created UDP server and tested on wireshark for sending messages

--- head part/esp32cam/esp32cam.ino
+++ head part/esp32cam/esp32cam.ino
@@ -1,9 +1,20 @@
 #include <WiFi.h>
 #include "soc/soc.h" //disable brownout problems
 #include "soc/rtc_cntl_reg.h"  //disable brownout problems
+#include <UDP.h>
 
+//network credentials
 const char* ssid = "Stain";
 const char* password = "b$VJ518175";
+
+//UDP
+const int udpPORT = 5000;
+const char* receiver_ip = "192.168.1.2";
+WiFiUDP udp_socket;
+
+//test message
+const char* test = "test";
+int msg_size = sizeof(test);
 
 void connect_wifi(const char* ssid, const char* password){
    WiFi.mode(WIFI_STA);
@@ -25,8 +36,10 @@ void connect_wifi(const char* ssid, const char* password){
 void setup() {
   Serial.begin(115200);
   delay(100);
-
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable   detector
   connect_wifi(ssid, password);
+
+  udp_socket.begin(udpPORT);
 }
 
 void loop() {
@@ -34,4 +47,8 @@ void loop() {
       Serial.println("WIFI Disconnected. Attempting to reconnect: ");
       connect_wifi(ssid, password);
     }
+
+   udp_socket.beginPacket(receiver_ip, udpPORT);
+   udp_socket.write(reinterpret_cast<const uint8_t*>(test), msg_size);
+   udp_socket.endPacket();
 }
